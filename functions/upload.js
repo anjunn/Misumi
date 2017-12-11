47ae0970efd66b419d1c662211867000a5b4ff97
praveen <praveen@qburst.com>
2017-12-11 20:48:08 +0530

    # drag and drop animation

--- functions/upload.js
+++ functions/upload.js
@@ -8,13 +8,16 @@ let misumi = {
   upload: {
     value: function(url) {
       this.uploadId.waitForEnabled();
+
       browser.execute(function () {
         var inputElement = document.createElement('input');
         inputElement.type = 'file';
         inputElement.id = 'inputFileDragHandler';
         document.body.appendChild(inputElement);
       });
+
       browser.chooseFile('#inputFileDragHandler', url);
+
       browser.execute(function () {
         function FakeDataTransfer(file) {
           this.dropEffect = 'all';
@@ -26,23 +29,38 @@ let misumi = {
           };
           this.files = [file];
         };
+
         var fakeDropEvent;
+        var fakeDragEvent;
+
         if (document.createEvent) {
+          fakeDragEvent = document.createEvent("HTMLEvents");
+          fakeDragEvent.initEvent("dragenter", true, true);
           fakeDropEvent = document.createEvent("HTMLEvents");
-          fakeDropEvent.initEvent("drop", true, true)
+          fakeDropEvent.initEvent("drop", true, true);
         } else {
+          fakeDragEvent = document.createEventObject();
+          fakeDragEvent.eventType = "DragEvent";
           fakeDropEvent = document.createEventObject();
           fakeDropEvent.eventType = "DragEvent";
         }
+
         Object.defineProperty(fakeDropEvent, 'dataTransfer', {
           value: new FakeDataTransfer(document.getElementById('inputFileDragHandler').files[0])
         });
+
         var element = document.querySelector('.dragArea');
-        if (document.createEvent) {
-          element.dispatchEvent(fakeDropEvent);
-        } else {
-          element.fireEvent("on" + event.eventType, fakeDropEvent);
+
+        function dispatchEvent(fakeEvent) {
+          if (document.createEvent) {
+            element.dispatchEvent(fakeEvent);
+          } else {
+            element.fireEvent("on" + event.eventType, fakeEvent);
+          }
         }
+
+        dispatchEvent(fakeDragEvent);
+        window.setTimeout(() => dispatchEvent(fakeDropEvent), 1500);
       });
     }
   }
