6ed2c21b27102b6c9a4929ecf42f3bc809c5db1d
anju <anjun@qburst.com>
2018-02-23 17:24:38 +0530

    parts view hot fix

--- functions/project.js
+++ functions/project.js
@@ -730,8 +730,10 @@ let  projectPage = {
       fs.writeFile(writePath,"Failed Cases:"+"\n\n"+"Compare by taking material first, then surface tension\n"+"***************************************************************\n", function(err) {
           if (err) return console.log(err);
         });
-      this.thumbnail.waitForVisible();
-      this.thumbnail.click();
+      if(this.thumbnail.isVisible()){
+        this.thumbnail.waitForVisible();
+        this.thumbnail.click();
+      }
       browser.longWait();
       this.label.waitForVisible();
       var status, materialType, surfaceType,displayedSurfaceType,materialFromPartsView,z,s=1;
@@ -767,12 +769,14 @@ let  projectPage = {
         for(z=1;z<=materialLength;z++) {
           this.materialDropdownClick.waitForEnabled();
           this.materialDropdownClick.click();
+           materialFromPartsView=this.materialDropdown(z).getValue();
+          if(materialFromPartsView=="-999")
+            continue;
           var materialVariable = this.materialArray.getText().split('\n');
           materialLength=materialVariable.length;
           this.materialDropdown(z).waitForEnabled();
           console.log("...........................................................");
           this.materialDropdown(z).click();
-          materialFromPartsView=this.materialDropdown(z).getValue();
           materialDisplayName=this.materialDropdown(z).getText();
           for(i=start;i<end;i++){
             if(third_sheet.data[i][3]=="材質") {  
@@ -878,6 +882,7 @@ let  projectPage = {
         this.materialDropdownClick.click();
         browser.tinyWait();
         this.materialDropdown(6).click();
+        browser.mediumWait();
         this.itemDropDownClick.waitForVisible();
         this.itemDropDownClick.click();
         this.itemDropDown(w).click();
@@ -901,6 +906,8 @@ let  projectPage = {
         for(z=1;z<=surfaceTensionLength;z++) {
           this.surfaceTensionClick.waitForEnabled();
           this.surfaceTensionClick.click();
+          if(this.surfaceTensionDropdown(z).getText()=="-999")
+            continue;
           this.surfaceTensionDropdown(z).waitForVisible();
           this.surfaceTensionDropdown(z).click();
           console.log("...........................................................");
@@ -914,7 +921,7 @@ let  projectPage = {
                     var flagNotRecommended=0;
                     var flagNotSupported=0;
                     this.materialArray.waitForEnabled();
-                    browser.tinyWait();
+                    browser.mediumWait();
                     var materialVariable = this.materialArray.getText().split('\n');
                     materialLength=materialVariable.length;
                     if(third_sheet.data[i][11]=="ANY" && third_sheet.data[i][12]=="ANY")
